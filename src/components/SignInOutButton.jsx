4d09ff0a30a96187034a6cfeddfbbeaaae4417f6
Chickendooner <kaius.nikander@live.fi>
2022-09-13 10:58:03 +0300

    Button change

--- src/components/SignInOutButton.jsx
+++ src/components/SignInOutButton.jsx
@@ -1,12 +1,13 @@
 import { useAuth, useSigninCheck } from "reactfire";
 import { signInWithGoogle, signOut } from "../utils/firebase/auth";
+import Button from 'react-bootstrap/Button';
 
 export default function SignInOutButton() {
   const { data: signInCheckResult } = useSigninCheck();
   const auth = useAuth();
 
   return signInCheckResult.signedIn ? (
-    <button onClick={() => signOut(auth)}>Sign out</button>
+    <Button variant="danger" onClick={() => signOut(auth)}>Sign out</Button>
   ) : (
     <button onClick={() => signInWithGoogle(auth)}>Sign in with Google</button>
   );
