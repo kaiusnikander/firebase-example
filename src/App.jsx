70411b934527899c9e09503e66a7782a25a09156
Chickendooner <kaius.nikander@live.fi>
2022-09-13 10:16:35 +0300

    Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useSigninCheck } from "reactfire";
 import Channel from "./components/Channel";
 import SignInOutButton from "./components/SignInOutButton";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   const { status, data: signInCheckResult } = useSigninCheck();
