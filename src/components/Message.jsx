a8aa2f6406461abac841c9c607f34d6769524e9f
Chickendooner <kaius.nikander@live.fi>
2022-09-20 09:01:00 +0300

    Delete own text

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,3 +1,8 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { useFirestore, useUser } from "reactfire";
+import Button from 'react-bootstrap/Button';
+
+
 const dateTimeFormat = new Intl.DateTimeFormat("en-GB", {
   hour: "numeric",
   minute: "numeric",
@@ -5,7 +10,10 @@ const dateTimeFormat = new Intl.DateTimeFormat("en-GB", {
   hour12: false,
 });
 
-export default function Message({ createdAt, text, displayName }) {
+export default function Message({ createdAt, text, displayName, id, uid }) {
+  const { data: user } = useUser();
+  const firestore = useFirestore();
+
   return (
     <div>
       [
@@ -19,6 +27,17 @@ export default function Message({ createdAt, text, displayName }) {
         {">"}
       </strong>{" "}
       {text}
+      {uid === user.uid ? (
+        <>
+        <Button
+        variant="outline-warning" onClick={async () => {
+            await deleteDoc(doc(firestore, "messages", id));
+          }}
+        >
+          Delete
+        </Button>{' '}
+        </>
+      ) : null}
     </div>
   );
 }
