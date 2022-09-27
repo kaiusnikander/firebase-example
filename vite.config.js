f97a8e978d4088b83ca13b11a1952ceeb57d258d
Chickendooner <kaius.nikander@live.fi>
2022-09-27 09:44:21 +0300

    PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: process.env.CI ? "/firebase-example/" : undefined,
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })], 
 });
