bd370104fbb11382bc7c3241a0f6b7a2ccbb7249
Chickendooner <kaius.nikander@live.fi>
2022-09-27 12:58:53 +0300

    PWA change

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,18 @@ export default defineConfig({
     short_name: 'MyApp',
     description: 'My Awesome App description',
     theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
   }
 })
 ], 
